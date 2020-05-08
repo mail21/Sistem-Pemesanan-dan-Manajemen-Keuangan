6d84c8682e5d1bb97534d620bc448133d06ac284
mail21 <ismailbito21@gmail.com>
2020-05-08 15:30:20 +0700

    Melakukan perbuahan besar

--- index.php
+++ index.php
@@ -1,5 +1,7 @@
 <?php 
-	$db = mysqli_connect("localhost","root","","restoran");
+	
+	include "koneksi.php";
+
 	$query = mysqli_query($db,"SELECT * FROM meja");
 	while ($query2 = mysqli_fetch_assoc($query)) {
 		$box[] = $query2;
@@ -11,114 +13,111 @@
 <head>
 	<meta charset="UTF-8">
 	<title>Home</title>
-	<link rel="stylesheet" href="./bootstrap-4.4.1-dist/css/bootstrap.css">
+	 <!-- Bootstrap core CSS -->
+	 <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+	<link rel="stylesheet" href="assets/styles/style.css">
+	<link rel="stylesheet" href="assets/styles/sidebar.css">
 </head>
-<style>
-	.banner{
-		width: 100%;
-		background-image: url("gambar.jpg");
-		height: 25vh;
-		background-size: cover;
-	}
-
-	.meja{
-		display: inline-block;
-		width: 100px;
-		height: 100px;
-		border: 1px solid;
-	}
-
-	.none{
-		display: none;
-	}
-
-</style>
 
 <body>
-	<div class="container-fluid banner"></div>
-	<nav class="navbar navbar-expand-lg navbar-light bg-light">
-	  
-	  <div class="collapse navbar-collapse" id="navbarNavDropdown">
-	    <ul class="navbar-nav">
-	      <li class="nav-item active">
-	        <a class="nav-link" href="index.php">Home <span class="sr-only">(current)</span></a>
-	      </li>
-	      <li class="nav-item">
-	        <a class="nav-link" href="halamanMenu.php">Pesan</a>
-	      </li>
-	      <li class="nav-item">
-	        <a class="nav-link" href="#">Laporan</a>
-	      </li>
-	    </ul>
-	  </div>
-	</nav>
-
-	<div class="container-fluid">
-		<?php 
-			foreach ($box as $meja) {
-				if ($meja['status'] === "kosong") {
-					echo "<div class='meja mr-2 btn-secondary' data-id='".$meja['id_meja']."' data-toggle='modal' data-target='#ModalAktif'> <span class='id'>".$meja['id_meja']."</span>". $meja['status'] ."</div>";	
-				}else if($meja['status'] === "reservasi"){
-					echo "<div class='meja mr-2 btn-primary' data-toggle='modal' data-target='#ModalAktif'> <span class='id'>".$meja['id_meja']."</span>". $meja['status'] ."</div>";
-				}else{
-					$menu = [];
-					$id = $meja['id_meja'];
-					$menuQuery = mysqli_query($db,"SELECT * FROM order_temp JOIN menu ON order_temp.id_menu = menu.id_menu WHERE id_meja = ".$meja['id_meja']);
-					while ($query3 = mysqli_fetch_assoc($menuQuery)) {
-						$menu[] = $query3;
-					}
-					$str = "";
-					$a = 0;
-					foreach ($menu as $menus) {
-						if ($a>0) {
-							$str .=",";
-						}
-						$nama = $menus['nama'];
-						$qt = $menus['quantity'];
-						$total = $menus['total'];
-						$ket = preg_replace('/\s+/', '', $menus['ket']);
-					$str .= "{'nama':'$nama','quantity':$qt,'total':$total,'ket':'$ket'}";
-						// echo $menus['nama'];
-						// echo $menus['quantity'];
-						// echo $menus['total'];
-						// echo $menus['ket'];
-					$a++;
-					}
-					echo $a;
-					echo $str;
-					echo "<div class='meja mr-2 btn-warning' data-toggle='modal' data-target='#ModalAktif'  data-menu=[$str]> <span class='id'>".$meja['id_meja']."</span>". $meja['status'] ."</div>";
-				}
-			}
-		 ?>
-	</div>
-	
-	
-
-
-<!-- Modal Pesanan Aktif -->
-<div class="modal fade" id="ModalAktif" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-  <div class="modal-dialog" role="document">
-    <div class="modal-content">
-      <div class="modal-header">
-        <h5 class="modal-title" id="exampleModalLabel">Order</h5>
-        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-          <span aria-hidden="true">&times;</span>
-        </button>
-      </div>
-      <div class="modal-body">
-
-      </div>
-      <div class="modal-footer">
-        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-        <button type="button" class="btn btn-primary">Save changes</button>
+<div class="d-flex" id="wrapper">
+	<!-- Sidebar -->
+    <div class="bg-light border-right" id="sidebar-wrapper">
+      <div class="sidebar-heading">Welcome , user </div>
+      <div class="list-group list-group-flush">
+        <a href="index.php" class="list-group-item list-group-item-action bg-light">Home</a>
+        <a href="menu.php" class="list-group-item list-group-item-action bg-light">Pesan</a>
+        <a href="laporan.php" class="list-group-item list-group-item-action bg-light">Laporan</a>
       </div>
     </div>
-  </div>
+	<!-- /#sidebar-wrapper -->
+	
+	<!-- Page Content -->
+    <div id="page-content-wrapper">
+
+		<div class="container-fluid banner"></div>
+      <nav class="navbar navbar-expand-lg navbar-light bg-light border-bottom">
+        <button class="btn btn-primary" id="menu-toggle">Toggle Menu</button>
+
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+      </nav>
+	
+
+		<div class="container-fluid">
+			<?php 
+				foreach ($box as $meja) {
+					if ($meja['status'] === "kosong") {
+						echo "<div class='meja mr-2 btn-secondary' data-id='".$meja['id_meja']."' data-toggle='modal' data-target='#ModalAktif'> <span class='id'>".$meja['id_meja']."</span>". $meja['status'] ."</div>";	
+					}else if($meja['status'] === "reservasi"){
+						echo "<div class='meja mr-2 btn-primary' data-toggle='modal' data-target='#ModalAktif'> <span class='id'>".$meja['id_meja']."</span>". $meja['status'] ."</div>";
+					}else{
+						$menu = [];
+						$id = $meja['id_meja'];
+						$menuQuery = mysqli_query($db,"SELECT * FROM order_list JOIN menu ON order_list.id_menu = menu.id_menu WHERE id_meja = ".$meja['id_meja']);
+						while ($query3 = mysqli_fetch_assoc($menuQuery)) {
+							$menu[] = $query3;
+						}
+						$str = "";
+						$a = 0;
+						foreach ($menu as $menus) {
+							if ($a>0) {
+								$str .=",";
+							}
+							$nama = $menus['nama'];
+							$qt = $menus['quantity'];
+							$total = $menus['total'];
+							$ket = preg_replace('/\s+/', '', $menus['ket']);
+						$str .= "{'nama':'$nama','quantity':$qt,'total':$total,'ket':'$ket'}";
+							// echo $menus['nama'];
+							// echo $menus['quantity'];
+							// echo $menus['total'];
+							// echo $menus['ket'];
+						$a++;
+						}
+						echo $str;
+						echo "<div class='meja mr-2 btn-warning' data-toggle='modal' data-target='#ModalAktif'  data-menu=[$str]> <span class='id'>".$meja['id_meja']."</span>". $meja['status'] ."</div>";
+					}
+				}
+			?>
+		</div>
+	
+	</div>
+    <!-- /#page-content-wrapper -->
+
+
+	<!-- Modal Pesanan Aktif -->
+	<div class="modal fade" id="ModalAktif" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+	<div class="modal-dialog" role="document">
+		<div class="modal-content">
+		<div class="modal-header">
+			<h5 class="modal-title" id="exampleModalLabel">Order</h5>
+			<button type="button" class="close" data-dismiss="modal" aria-label="Close">
+			<span aria-hidden="true">&times;</span>
+			</button>
+		</div>
+		<div class="modal-body">
+
+		</div>
+		<div class="modal-footer">
+			<button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+			<button type="button" class="btn btn-primary">Save changes</button>
+		</div>
+		</div>
+	</div>
+	</div>
+	<!-- Modal Pesanan Aktif -->
+	
 </div>
-	
-	
 </body>
+<script src="vendor/jquery/jquery.min.js"></script>
+<script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
 <script>
+	$("#menu-toggle").click(function(e) {
+      e.preventDefault();
+      $("#wrapper").toggleClass("toggled");
+    });
 	
 	// =======================   meja ===============================
 	let rows = document.querySelectorAll(".meja");
@@ -164,7 +163,7 @@
 		})
 	}
 </script>
-<script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+<!-- <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
-<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script> -->
 </html>
--- menu.php
+++ menu.php
@@ -1,22 +1,176 @@
 <?php 
-	
-	// $db = mysqli_connect("localhost","root","","restoran");
+	include "koneksi.php";
+	$query = mysqli_query($db,"SELECT * FROM meja");
+	while ($query2 = mysqli_fetch_assoc($query)) {
+		$box[] = $query2;
+	}
 
-	// $query = mysqli_query($db,"SELECT * FROM meja");
-	// while ($query2 = mysqli_fetch_assoc($query)) {
-	// 	$box[] = $query2;
-	// }
-	// if ($box[0]['status'] === "aktif") {
-	// 	$warna = "red";
-	// }else{
-	// 	$warna = "blue";
-	// }
-	// echo "<div style='background-color: ".$warna."'> ".$box[0]['status']." </div>";
-	
-	// echo "<br>";
-	// var_dump($_GET);
-	$a = date("Y-m-d");
-	var_dump($a);
+ ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Home</title>
+  <!-- Bootstrap core CSS -->
+  <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+  <link rel="stylesheet" href="assets/styles/style.css">
+  <link rel="stylesheet" href="assets/styles/sidebar.css">
+</head>
 
-?>
-<a href="decoy.php">asdas</a>
+
+<body>
+
+<div class="d-flex" id="wrapper">
+	<!-- Sidebar -->
+    <div class="bg-light border-right" id="sidebar-wrapper">
+      <div class="sidebar-heading">Welcome , user </div>
+      <div class="list-group list-group-flush">
+	  	<a href="index.php" class="list-group-item list-group-item-action bg-light">Home</a>
+        <a href="menu.php" class="list-group-item list-group-item-action bg-light">Pesan</a>
+        <a href="laporan.php" class="list-group-item list-group-item-action bg-light">Laporan</a> 
+	  </div>
+    </div>
+	<!-- /#sidebar-wrapper -->
+	
+	<!-- Page Content -->
+    <div id="page-content-wrapper">
+
+		<div class="container-fluid banner"></div>
+      <nav class="navbar navbar-expand-lg navbar-light bg-light border-bottom">
+        <button class="btn btn-primary" id="menu-toggle">Toggle Menu</button>
+
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+      </nav>
+
+      
+	  <div class="container-fluid">
+		  <?php 
+			  $mejaQuery = "SELECT * FROM reservasi JOIN meja ON reservasi.id_reservasi = meja.id_reservasi";
+			  $query4 = mysqli_query($db,$mejaQuery);
+			  while ($query5 = mysqli_fetch_assoc($query4)) {
+				  $boxMeja[] = $query5;
+			  }
+			  $menuQuery = "SELECT * FROM menu";
+			  $query6 = mysqli_query($db,$menuQuery);
+			  while ($query7 = mysqli_fetch_assoc($query6)) {
+				  $boxMenu[] = $query7;
+			  }
+			  $strMeja = "";
+			  $b = 0;
+			  foreach ($boxMeja as $MEJA) {
+				  if($b>0){
+					  $strMeja .= ",";
+				  }
+						  $id_meja = $MEJA['id_meja'];
+						  $nama_pelanggan = $MEJA['nama_pelanggan'];
+						  $id_reservasi = $MEJA['id_reservasi'];
+						  $status = $MEJA['status'];
+				  $strMeja .= "{'id_meja':'$id_meja','nama_pelanggan':'$nama_pelanggan','id_reservasi':$id_reservasi,'status':'$status'}";
+				  $b++;	
+			  }
+			  var_dump($strMeja);
+			  echo "<input type='hidden' id='hiddenMeja' value=[$strMeja]>";
+			  foreach ($boxMenu as $menuData) {
+				  echo "<div class='row menu' style='border : 1px solid' data-toggle='modal' data-target='#ModalMenu'>
+				  ".$menuData['id_menu'] ." 
+				  <br>
+				   ".$menuData['nama'] ."
+				   <br>
+				   ".$menuData['gambar'] ."
+				   <br>
+				   ".$menuData['harga'] ."
+				   <br>
+				   </div>";
+			  }
+		   ?>
+		   
+	  </div>
+
+    </div>
+    <!-- /#page-content-wrapper -->
+
+	<!-- Modal Pesanan Kosong -->
+	<div class="modal fade" id="ModalMenu" tabindex="-1" role="dialog">
+	  <div class="modal-dialog" role="document">
+	    <div class="modal-content">
+	      <div class="modal-header">
+	        <h5 class="modal-title">Pesan </h5>
+	        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+	          <span aria-hidden="true">&times;</span>
+	        </button>
+	      </div>
+	      <div class="modal-body2">
+			
+	      </div>
+	      <div class="modal-footer">
+	        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+	        <button type="button" class="btn btn-primary">Save changes</button>
+	      </div>
+	    </div>
+	  </div>
+	</div>
+	<!-- Modal Pesanan Kosong -->	
+
+</div>
+
+</body>
+<!-- Bootstrap core JavaScript -->
+<script src="vendor/jquery/jquery.min.js"></script>
+<script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+	<script>
+	
+		$("#menu-toggle").click(function(e) {
+		  e.preventDefault();
+		  $("#wrapper").toggleClass("toggled");
+		});
+		
+		let menuRows = document.querySelectorAll(".menu");
+		for (menuRow of menuRows) {
+			 menuRow.addEventListener("click",async ()=>{
+				 const modalBody2 = document.querySelector('.modal-body2');
+					 let noMeja = document.querySelector("#hiddenMeja");
+					 let strMejaValue = noMeja.value;
+					 strMejaValue = strMejaValue.replace(/\'/g, '"');
+					 const dataMeja = JSON.parse(strMejaValue);
+					 console.log(dataMeja)
+					 let str2 = ""
+					 for ( let strDataMeja of dataMeja) {
+						 if(strDataMeja.nama_pelanggan != "kosong"){
+						 str2 += `
+						<option value='${strDataMeja.id_meja}'>${strDataMeja.id_meja} - ${strDataMeja.status} - ${strDataMeja.nama_pelanggan} </option>`;
+						 }else{
+							 str2 += `
+						<option value='${strDataMeja.id_meja}'>${strDataMeja.id_meja} - ${strDataMeja.status}</option>
+						 `
+						 }	
+					 }
+					 let isi2 = `
+	<form action="ubah.php" method="GET">
+					No Meja : <select name="noMeja" id="select">
+						${str2}
+					</select>
+					
+					<br>
+					<input type="hidden" name="id" value="">
+					harga : <input type="text" name="harga">
+					<br>
+					Keterangan : <input type="text" name="ket">
+					<button name="submit">submit</button>
+	</form>
+					 `
+					 modalBody2.innerHTML = isi2;
+					 let selectTag = document.querySelector("#select")
+					console.log(selectTag)
+					selectTag.addEventListener("change",(e)=>{
+						console.log(e.target.value)
+					})
+			 })
+		 } 
+	
+	</script>
+<!-- <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script> -->
+</html>
